a9be9ecafd6f3d503947cd6f09a40086b576de6e
Arraazzii <mumaraziz2014@gmail.com>
2019-01-05 02:59:23 +0700

    bacot

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,30 +1,33 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-absolute navbar-transparent" data-color="blue">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle d-inline">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand" href="#pablo">Dashboard</a>
+        <div class="container">
+                <div class="navbar-wrapper">
+                        <div class="navbar-toggle d-inline">
+                                <button type="button" class="navbar-toggler">
+                                        <span class="navbar-toggler-bar bar1"></span>
+                                        <span class="navbar-toggler-bar bar2"></span>
+                                        <span class="navbar-toggler-bar bar3"></span>
+                                </button>
+                        </div>
+                        <a class="navbar-brand" href="#pablo">Dashboard</a>
+                </div>
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button> -->
+                <div class="photo">
+                        <img src="<?php echo base_url('assets/img/anime3.png')?>" data-toggle="collapse" data-target="#navigation">
           </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navigation">
-            <ul class="navbar-nav ml-auto ">
-              <div class="search-bar input-group">
-                <!-- <input type="text" class="form-control" placeholder="Search...">
+                        <div class="collapse navbar-collapse" id="navigation">
+                                <ul class="navbar-nav ml-auto ">
+                                        <!-- <div class="search-bar input-group"> -->
+                                        <!-- <input type="text" class="form-control" placeholder="Search...">
       <div class="input-group-addon"><i class="tim-icons icon-zoom-split"></i></div> -->
-                <button class="btn btn-link" id="search-button" data-toggle="modal" data-target="#searchModal"><i class="tim-icons icon-zoom-split"></i></button>
-                <!-- You can choose types of search input -->
-              </div>
-              <!-- <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
+                                        <!-- <button class="btn btn-link" id="search-button" data-toggle="modal" data-target="#searchModal"><i class="tim-icons icon-zoom-split"></i></button> -->
+                                        <!-- You can choose types of search input -->
+                                        <!-- </div> -->
+                                        <!-- <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -39,7 +42,7 @@
       </div>
     </div>
   </div> -->
-              <li class="dropdown nav-item">
+                                        <!-- <li class="dropdown nav-item">
                 <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                   <div class="notification d-none d-lg-block d-xl-block"></div>
                   <i class="tim-icons icon-sound-wave"></i>
@@ -64,17 +67,8 @@
                     <a href="#" class="nav-item dropdown-item">Another one</a>
                   </li>
                 </ul>
-              </li>
-              <li class="dropdown nav-item">
-                <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
-                  <div class="photo">
-                    <img src="<?php echo base_url('assets/img/anime3.png')?>">
-                  </div>
-                  <b class="caret d-none d-lg-block d-xl-block"></b>
-                  <p class="d-lg-none">
-                    Log out
-                  </p>
-                </a>
+              </li> -->
+                                        <!-- <li class="dropdown nav-item">
                 <ul class="dropdown-menu dropdown-navbar">
                   <li class="nav-link">
                     <a href="#" class="nav-item dropdown-item">Profile</a>
@@ -87,22 +81,34 @@
                     <a href="#" class="nav-item dropdown-item">Log out</a>
                   </li>
                 </ul>
-              </li>
-              <li class="separator d-lg-none"></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+              </li> -->
+                                        <ul class="navbar-nav">
+                                                <li class="nav-item active">
+                                                        <a class="nav-link" href="#">Profile<span class="sr-only">(current)</span></a>
+                                                </li>
+                                                <li class="nav-item">
+                                                        <a class="nav-link" href="#">Setting</a>
+                                                </li>
+                                                <div class="dropdown-divider"></div>
+                                                <li class="nav-item">
+                                                        <a class="nav-link" href="#">Logout</a>
+                                                </li>
+                                        </ul>
+                                        <li class="separator d-lg-none"></li>
+                                </ul>
+                        </div>
+                </div>
+</nav>
 <!-- End Navbar -->
 <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
         <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="SEARCH">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <i class="tim-icons icon-simple-remove"></i>
-              </button>
-            </div>
-          </div>
+                <div class="modal-content">
+                        <div class="modal-header">
+                                <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="SEARCH">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <i class="tim-icons icon-simple-remove"></i>
+                                </button>
+                        </div>
+                </div>
         </div>
-      </div>
+</div>
